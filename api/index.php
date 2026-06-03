87f3ec18d6facded30ec29bbab2bc538f4ba98b5
faizsz <faizakmalld@gmail.com>
2026-06-03 23:01:03 +0700

    fix api routing prefix on vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,6 @@
 <?php
+// Force SCRIPT_NAME to /index.php so Laravel doesn't detect /api as the base path on Vercel
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
 require __DIR__ . '/../public/index.php';
